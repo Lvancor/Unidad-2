eecc42c37f78bf995b8a47fcf2b5c00932d95ea8
Lucia Ainhoa <LVANCOR@G.EDUCAAND.ES>
2025-01-31 11:48:37 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,9 @@
+function Calcular() {
+    let a;
+   let b;
+   a= parseInt(prompt("Introduce un número:"),10);
+   b= parseInt(prompt("Introduce un número:"),10);
+  
+   alert("el primer numero es: " + a + "\nEl segundo numero es: " + b + "\nLa suma es: " + ( a + b ) + "\nLa resta es: " + (a - b) + "\nEl producto es: " + ( a * b ) + " \nLa división es: " + ( a / b ) +"\nEl resto es: " + ( a % b ));
+
+}
