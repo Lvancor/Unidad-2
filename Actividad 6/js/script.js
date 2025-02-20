b34b45cb7386f34ac5b9def803ff3d443405d872
Lucia Ainhoa <LVANCOR@G.EDUCAAND.ES>
2025-02-20 14:09:54 +0100

    Add files via upload

---
+++ Actividad 6/js/script.js
@@ -0,0 +1,12 @@
+function comprobar() {
+   let numero ;
+   numero = parseInt(document.getElementById ("numero").value);
+   
+   if (numero % 2 == 0 ) {
+      document.getElementById("mensaje").innerHTML = "El númeron es par";
+   }
+   else {
+      document.getElementById("mensaje").innerHTML = "El número es impar";
+   }
+        
+}
