03eee37b1092748bea2a9db68d77eeff170860d9
Lucia Ainhoa <LVANCOR@G.EDUCAAND.ES>
2025-02-24 19:11:15 +0100

    Update script.js

--- Actividad 6/js/script.js
+++ Actividad 6/js/script.js
@@ -3,7 +3,7 @@ function comprobar() {
    numero = parseInt(document.getElementById ("numero").value);
    
    if (numero % 2 == 0 ) {
-      document.getElementById("mensaje").innerHTML = "El númeron es par";
+      document.getElementById("mensaje").innerHTML = "El número es par";
    }
    else {
       document.getElementById("mensaje").innerHTML = "El número es impar";
